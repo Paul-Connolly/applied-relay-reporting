a132d8c265140fe8b329f328a00437ac75f919e6
unknown <paul.connolly@appliedsystems.com>
2018-02-01 22:24:18 +0000

    homepage start

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/theme/style.css';
 import App from './components/App.js';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './app/index.html',
     filename: 'index.html',
     inject: 'body'
-})
+});
 
 module.exports = {
     entry: './app/index.js',
@@ -17,7 +17,16 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, loader: ['style-loader', 'css-loader']},
+            { 
+                test: require.resolve('jquery'), 
+                use: [
+                    {loader: 'expose-loader', options: 'jQuery'},
+                    {loader: 'expose-loader', options: '$'}
+                ]
+            },
+            { test: require.resolve('tether'), loader: 'expose-loader', options: 'Tether'}
         ]
     },
     plugins: [HtmlWebpackPluginConfig]
