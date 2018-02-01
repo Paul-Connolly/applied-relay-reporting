661a6da0d775d52fea60a1ed47c8fb5b805c2696
unknown <paul.connolly@appliedsystems.com>
2018-02-01 21:00:01 +0000

    env setup

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center', textDecoration: 'underline'}}>
+                <h1>Applied Relay Reporting</h1>
+            </div>
+        );
+    }
+}
---
+++ app/index.js
@@ -0,0 +1,7 @@
+/* this is the webpack entry file */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
